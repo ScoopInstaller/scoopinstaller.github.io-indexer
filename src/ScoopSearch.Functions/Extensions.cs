27ea09b8d5a1bcf00f39f1b12f1df0036c5b365c
Gregoire Pailler <gregoire@pailler.fr>
2021-11-14 23:28:46 +0800

    Replace Task.WhenAll and manual tasks creation with Parallel.ForEachAsync

---
+++ src/ScoopSearch.Functions/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScoopSearch.Functions
+{
+    public static class Extensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> @this, Action<T> action)
+        {
+            foreach (var element in @this)
+            {
+                action(element);
+            }
+        }
+    }
+}
