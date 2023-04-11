d595521d73bcc58c99e228e38d7a9167a2d540db
Gregoire Pailler <gregoire@pailler.fr>
2023-04-11 21:10:43 +0800

    Add OfficialRepositoryNumber for unit tests

--- src/ScoopSearch.Functions.Tests/Helpers/ManifestInfoExtensions.cs
+++ src/ScoopSearch.Functions.Tests/Helpers/ManifestInfoExtensions.cs
@@ -7,6 +7,6 @@ public static class ManifestInfoExtensions
 {
     public static ManifestInfo ToManifestInfo(this (string Id, string Sha, int RepositoryStars) @this)
     {
-        return JsonSerializer.Deserialize<ManifestInfo>(@$"{{ ""Id"": ""{@this.Id}"", ""Metadata"": {{ ""Sha"": ""{@this.Sha}"", ""RepositoryStars"": {@this.RepositoryStars} }} }}")!;
+        return JsonSerializer.Deserialize<ManifestInfo>(@$"{{ ""Id"": ""{@this.Id}"", ""Metadata"": {{ ""Sha"": ""{@this.Sha}"", ""RepositoryStars"": {@this.RepositoryStars}, ""OfficialRepositoryNumber"": 0 }} }}")!;
     }
 }
